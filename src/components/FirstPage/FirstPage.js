435c08b52058de0248a2144c718ac2f9ad7d9011
kamel beloula <kamel.solo@live.com>
2023-03-23 22:18:35 +0000

    playing with the background again

--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -1,57 +1,61 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Animation from '../Animation/Animation';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Animation from "../Animation/Animation";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 
-import './FirstPage.css'
+import "./FirstPage.css";
 
 function FirstPage(props) {
   // const getCurrentActivites = props.getCurrentActivities
 
-  return ( //returns the animation/logo, quote and button on the first page
+  return (
+    //returns the animation/logo, quote and button on the first page
 
-    <div >
+    <div>
       <div id="frame">
         <Animation></Animation>
-        {/* <Container className="btnContainer">
-          <Row>
-            <Col className="text-center" style ={{ marginTop: "30em"}}>
-        
-        </Col>
-          </Row>
-      </Container> */}
+
         {/* <p id='quote'>"There are no uninteresting things, only uninterested people."
           G.K. Chesterton</p> */}
       </div>
       <div class="context">
-        <h4>"There are no uninteresting things, only uninterested people."
-          G.K. Chesterton</h4>
-        <Button id='button' onClick={props.getCurrentActivites}>
-          <span><FaArrowRight />  Click Here  <FaArrowLeft />
-          </span>
-        </Button>
-    </div>
+        <h4>
+          "There are no uninteresting things, only uninterested people." G.K.
+          Chesterton
+        </h4>
+        <Container className="btnContainer">
+          <Row>
+            <Col className="text-center" style={{ marginTop: "30em" }}>
+              <Button id="button" onClick={props.getCurrentActivites}>
+                <span>
+                  <FaArrowRight /> Click Here <FaArrowLeft />
+                </span>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
 
-<div class="area" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default FirstPage;
